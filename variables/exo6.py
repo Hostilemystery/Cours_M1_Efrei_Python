05217fd801c061d99c18f9a4e48c3f7d9abffbe5
NANJI FREDDY <nanjifreddy2@gmail.com>
2024-04-24 14:34:51 +0200

    les variables

--- variables/exo6.py
+++ variables/exo6.py
@@ -4,6 +4,8 @@ correspondant au mot suivant : “quarante-deux”.
 
 """
 
-myArray42 = ['q','u','a','r','a','n','t','e','-','d','e','u','x']
+#myArray42 = ['q','u','a','r','a','n','t','e','-','d','e','u','x']
+myArray42 = [*'quarante-deux']   #list("quarante-deux")
+
 
 print(myArray42)
